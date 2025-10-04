639010da4f2df32c1c222c604d7250ebb3fb56f9
Margarita <margarita0work@gmail.com>
2025-10-04 19:31:38 +0300

    Added waitlist page with Google Form, removed SignIn/SignOut 2

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import ButtonLogout from "@/components/ButtonLogout";
+// import ButtonLogout from "@/components/ButtonLogout";
 import { auth } from "@/auth";
 
 export default async function Dashboard() {
@@ -7,11 +7,11 @@ export default async function Dashboard() {
 
   return (
     <main className="bg-custom-gradient min-h-screen flex flex-col items-center justify-center text-center px-5 sm:px-14 lg:px-20 relative">
-      <ButtonLogout className="absolute top-5 left-5">Sign Out</ButtonLogout>
+      {/* <ButtonLogout className="absolute top-5 left-5">Sign Out</ButtonLogout> */}
 
       <div className="max-w-2xl w-full">
         <h1 className="font-bold text-[clamp(38px,4.6vw,42px)] sm:text-[clamp(48px,4.6vw,60px)] leading-[1.2] mb-6 text-base-content">
-          Thanks for joining the waitlist, {session?.user?.name || "friend"}!
+          {/* Thanks for joining the waitlist, {session?.user?.name || "friend"}! */}
         </h1>
         <p className="font-primary text-base-content/70 lg:text-xl sm:text-lg text-base mb-12 max-w-lg mx-auto">
           Your dashboard is in progress. Everything will be ready for you before

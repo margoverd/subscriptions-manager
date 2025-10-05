5f92f09c814500370a5be574a3fa96bf8d4de3ea
Margarita <margarita0work@gmail.com>
2025-10-05 13:15:27 +0300

    Added alternative registration link for waitlist page

--- app/waitlist/page.js
+++ app/waitlist/page.js
@@ -7,18 +7,30 @@ export default function Waitlist() {
     <>
       <Header />
       <main className="mt-13 sm:pb-27 pb-14 sm:pt-28 pt-16 *:bg-custom-gradient min-h-screen flex flex-col items-center justify-center text-center px-5 sm:px-14 lg:px-20 relative">
-        <div className="max-w-2xl w-full">
+        <div className="w-full">
           <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLSdg4oK71AoKVffrCGgD_N6t_6VWXOdmpQadZXMs4KBfsg_J9g/viewform?embedded=true"
-            width="640"
-            height="472"
+            width="100%"
+            height="570"
+            className="scrollbar-custom"
           >
             Loading…
           </iframe>
 
+          <p className="mt-4">
+            Not seeing anything?{" "}
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdg4oK71AoKVffrCGgD_N6t_6VWXOdmpQadZXMs4KBfsg_J9g/viewform?usp=header"
+              className="underline"
+              target="_blank"
+            >
+              Join through this link
+            </a>
+          </p>
+
           <Link
             href="/"
-            className="btn btn-primary rounded-lg shadow-none py-2 px-6 sm:py-3 sm:px-8 bg-primary hover:opacity-70 transition font-normal text-base-content text-base sm:text-lg mt-8"
+            className="btn btn-primary rounded-lg shadow-none py-2 px-6 sm:py-3 sm:px-8 bg-primary hover:opacity-70 transition font-normal text-base-content text-base sm:text-lg mt-14"
           >
             Back to Home
           </Link>

4036e638c06f8b2ee1ef90293e2b6a95c9cfdb4e
Margarita <margarita0work@gmail.com>
2026-01-23 12:15:40 +0200

    feat: add statistic page

---
+++ app/statistic/layout.js
@@ -0,0 +1,20 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { SessionProvider } from "@/components/SessionProvider";
+
+export default async function LayoutPrivate({ children }) {
+  const session = await auth();
+  // if person didn't signUp or session expired, it will be undefined
+  if (!session) {
+    // redirect person
+    redirect("/");
+  }
+
+  return (
+    <SessionProvider value={session}>
+      <div className="bg-base-200 min-h-screen relative overflow-y-auto">
+        {children}
+      </div>
+    </SessionProvider>
+  );
+}
